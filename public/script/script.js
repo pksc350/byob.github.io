a878daa277e3317c640b10cef714c52190a708fd
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-02-03 08:24:39 -0800

    Functions working to add ingredients/steps and push to db

--- public/script/script.js
+++ public/script/script.js
@@ -6,46 +6,15 @@ const addIng = document.getElementById("add-ing");
 const addStep = document.getElementById("add-step");
 
 let addIngredient = addIng.addEventListener("click", (evt) => {
-  boxvalue = document.getElementById("ingbox").value;
-  let currentIng = ingredientList.push(boxvalue);
-
-  console.log(ingredientList);
-  clear();
-
-  ingredientList.forEach(function (i) {
-    let listItm = document.createElement("li");
-    let text = document.createTextNode(i);
-
-    listItm.appendChild(text);
-
-    document.getElementById("ing-list").appendChild(listItm);
-  });
-
-  ingredientList = [];
+  let listItm = document.createElement("div");
+  listItm.innerHTML =
+    '<input type="text" name="ingredients" id="ingbox" class="type-here">';
+  document.getElementById("ingredients").appendChild(listItm);
 });
 
 let addDirection = addStep.addEventListener("click", (evt) => {
-  dirValue = document.getElementById("dirbox").value;
-  let currentDir = dirList.push(dirValue);
-
-  clearDir();
-
-  dirList.forEach(function (d) {
-    let listItm = document.createElement("li");
-    let text = document.createTextNode(d);
-
-    listItm.appendChild(text);
-
-    document.getElementById("dir-list").appendChild(listItm);
-  });
-
-  dirList = [];
+  let listItm = document.createElement("div");
+  listItm.innerHTML =
+    '<input type="text" name="directions" id="dirbox" class="type-here">';
+  document.getElementById("directions").appendChild(listItm);
 });
-
-function clear() {
-  document.getElementById("ingbox").value = "";
-}
-
-function clearDir() {
-  document.getElementById("dirbox").value = "";
-}
