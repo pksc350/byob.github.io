be7e9b91d426fcf285e9875aeb946b5f8f457d87
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-02-03 18:28:33 -0800

    Reviews working - posting to drink details

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const reviewsCtrl = require("../controllers/reviews");
 const drinksCtrl = require("../controllers/drinks");
 
-router.post("/drinks/:id", reviewsCtrl.create);
+router.post("/drink/:id", reviewsCtrl.create);
 router.get("/drinks/:id", drinksCtrl.show);
 
 module.exports = router;
