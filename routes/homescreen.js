31b60c83c35633dd2fe9e83aec084a93e7d7006c
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-02-05 22:37:06 -0800

    added db

--- routes/homescreen.js
+++ routes/homescreen.js
@@ -16,7 +16,7 @@ router.get(
 router.get(
   "/oauth2callback",
   passport.authenticate("google", {
-    successRedirect: "drinks/new",
+    successRedirect: "/drinks",
     failureRedirect: "/",
   })
 );
