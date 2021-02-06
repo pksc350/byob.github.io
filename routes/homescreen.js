d25c63b17b0d3408e80976a5599b0132f4b8b8be
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-02-05 22:29:26 -0800

    trying something else

--- routes/homescreen.js
+++ routes/homescreen.js
@@ -16,7 +16,7 @@ router.get(
 router.get(
   "/oauth2callback",
   passport.authenticate("google", {
-    successRedirect: "/drinks/add",
+    successRedirect: "drinks/new",
     failureRedirect: "/",
   })
 );
